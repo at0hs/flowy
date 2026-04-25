92e81f1357fe8883e1de0f8eebbaba71c213cff7
4t0hs <atohs0tech@duck.com>
2026-04-26 02:36:18 +0900

    マイグレーションファイルを修正

--- supabase/migrations/20260422133011_v0_7_0.sql
+++ supabase/migrations/20260422133011_v0_7_0.sql
@@ -5,7 +5,7 @@ CREATE EXTENSION IF NOT EXISTS "pg_trgm" WITH SCHEMA extensions;
 -- ----------------------------------------
 CREATE INDEX tickets_assignee_id_idx ON tickets(assignee_id);
 CREATE INDEX tickets_updated_at_idx ON tickets(updated_at DESC);
-CREATE INDEX tickets_title_trgm_idx ON tickets USING gin(title gin_trgm_ops);
+CREATE INDEX tickets_title_trgm_idx ON tickets USING gin(title extensions.gin_trgm_ops);
 
 -- ----------------------------------------
 -- チケットにカテゴリを追加
